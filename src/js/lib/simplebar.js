06564c8eab64b5e8628e4609ee27fd636749e260
vyazemskaya <vyazemskayanna@gmail.com>
2026-03-01 17:55:16 +0300

    update

--- src/js/lib/simplebar.js
+++ src/js/lib/simplebar.js
@@ -1,14 +1,21 @@
-import SimpleBar from 'simplebar';
+import SimpleBar from "simplebar";
 // import 'simplebar/dist/simplebar.css';
 
 const initSimplebar = () => {
-  document.querySelectorAll('[data-sb]').forEach(el => {
-    if (!el.querySelector('.simplebar-wrapper')) {
+  document.querySelectorAll("[data-sb]").forEach(el => {
+    if (!el.querySelector(".simplebar-wrapper")) {
       new SimpleBar(el, {
         autoHide: false,
       });
     }
   });
+  if (document.querySelectorAll(".dropdown").length) {
+    document.querySelectorAll(".dropdown").forEach(el => {
+      new SimpleBar(el, {
+        autoHide: false,
+      });
+    });
+  }
 };
 
-window.addEventListener('load', initSimplebar);
+window.addEventListener("load", initSimplebar);
